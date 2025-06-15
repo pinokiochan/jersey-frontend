92832966e9d434023558b8667cc828ccfc5a6d7b
pinokiochan <beibarys7ergaliev@gmail.com>
2025-06-15 19:43:06 +0500

    feat: fixed size button on header "войти"

--- 12-jerseys/src/components/Header.jsx
+++ 12-jerseys/src/components/Header.jsx
@@ -224,7 +224,7 @@ export default function Header() {
             ) : (
               <Link
                 to="/login"
-                className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2"
+                className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-xl font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2"
               >
                 Войти
               </Link>
