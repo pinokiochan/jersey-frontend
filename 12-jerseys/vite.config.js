0a09f63edcf6f5874f3ebf72c02cdf4b09fdd7c0
pinokiochan <beibarys7ergaliev@gmail.com>
2025-07-13 21:52:38 +0500

    Set up for GitHub Pages

--- 12-jerseys/vite.config.js
+++ 12-jerseys/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 5173,
-    host: "localhost", // Показывать только localhost, убираем network адреса
+    allowedHosts: [
+      "all", // OR just allow the ngrok domain specifically:
+      "bc93-2-75-16-92.ngrok-free.app"
+    ],
+    strictPort: true
   },
 })
